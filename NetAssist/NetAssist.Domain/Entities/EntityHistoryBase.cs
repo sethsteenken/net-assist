11504020eb19bddd2fa70a84251da22f5837573e
Seth Steenken <sethsteenken@gmail.com>
2016-10-22 16:26:44 -0400

    Updated EntityHistoryBase to included non-generic class for EF. Updates to CommandDate.

--- NetAssist/NetAssist.Domain/Entities/EntityHistoryBase.cs
+++ NetAssist/NetAssist.Domain/Entities/EntityHistoryBase.cs
@@ -2,6 +2,31 @@
 
 namespace NetAssist.Domain
 {
+    public abstract class EntityHistoryBase : Entity<int>
+    {
+        protected EntityHistoryBase() { }
+
+        public EntityHistoryBase(int typeId, CommandTypeOption commandType, Guid entityId, string data, int user)
+        {
+            TypeId = typeId;
+            CommandTypeId = (int)commandType;
+            EntityId = entityId;
+            SerializedData = data.SetEmptyToNull();
+            Created = new CommandDate(user);
+        }
+
+        public Guid EntityId { get; private set; }
+        public int TypeId { get; private set; }
+        public virtual EntityTypeBase Type { get; private set; }
+
+
+        public int CommandTypeId { get; private set; }
+        public CommandTypeOption CommandType => (CommandTypeOption)CommandTypeId;
+
+        public string SerializedData { get; private set; }
+        public CommandDate Created { get; private set; }
+    }
+
     public abstract class EntityHistoryBase<T> : Entity<T>
     {
         protected EntityHistoryBase() { }
--- NetAssist/NetAssist.Domain/ValueObjects/CommandDate.cs
+++ NetAssist/NetAssist.Domain/ValueObjects/CommandDate.cs
@@ -2,24 +2,29 @@
 
 namespace NetAssist.Domain
 {
-    public class CommandDate : CommandDate<int> { }
+    public class CommandDate : CommandDate<int>
+    {
+        protected CommandDate() { }
+        public CommandDate(int user) : base (user) { }
+        public CommandDate(int user, DateTime date) : base (user, date) {  }
+    }
 
     public class CommandDate<T> : ValueObject<CommandDate<T>>
     {
         protected CommandDate() { }
 
-        public CommandDate(T userId) : this (userId, DateTime.UtcNow)
+        public CommandDate(T user) : this (user, DateTime.UtcNow)
         {
 
         }
 
-        public CommandDate(T userId, DateTime date)
+        public CommandDate(T user, DateTime date)
         {
-            UserId = userId;
+            User = user;
             Date = date;
         }
 
-        public T UserId { get; private set; }
+        public T User { get; private set; }
         public DateTime Date { get; private set; }
     }
 }
