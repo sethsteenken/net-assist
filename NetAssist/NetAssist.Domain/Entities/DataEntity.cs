5029453b49f95a1ff9669bdb813f6249d6f881af
Seth Steenken <sethsteenken@gmail.com>
2016-10-22 16:58:38 -0400

    Removed EntityHistory and EntityType due to too many complications with EF. Removed items to simplify DataEntity

--- NetAssist/NetAssist.Domain/Entities/DataEntity.cs
+++ NetAssist/NetAssist.Domain/Entities/DataEntity.cs
@@ -19,17 +19,5 @@ namespace NetAssist.Domain
 
         public virtual Guid Guid { get; protected set; }
         public virtual bool Archive { get; set; }
-
-        [SerializeIgnore]
-        public abstract EntityTypeBase EntityType { get; }
-
-        [SerializeIgnore]
-        public virtual bool LogHistory => true;
-
-        public void VerifyGuid()
-        {
-            if (Guid == Guid.Empty)
-                Guid = Guid.NewGuid();
-        }
     }
 }
